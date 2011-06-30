022e32e7cf4aa07638b71f70afbcb8d0ff5b2563
Wen-Tien Chang <ihower@gmail.com>
2011-06-30 21:34:50 +0800

    Initialize commit

---
+++ 2011/index.php
@@ -0,0 +1,1 @@
+<? header('Location:http://ruby.tw')?>
---
+++ index.php
@@ -0,0 +1,46 @@
+<html> 
+<head> 
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /> 
+<title>RubyConf Taiwan</title>
+<style> 
+  body { 
+    font-family: "Verdana", sans-serif;
+  }
+  .main {
+    width: 800px;
+    margin: auto;
+  }
+</style> 
+ 
+</head> 
+<body> 
+ 
+<div class="main"> 
+  <h1>RubyConf Taiwan</h1> 
+  <a href="http://www.flickr.com/search/?q=rubyconf%20taiwan"><img src="/images/rubyconf2010sc.jpg" width="800"></a>
+  
+  <ul> 
+    <li>2011/8/26-27, Taipei
+    	<ul>
+    		<li><a href="http://ruby.tw/post/5417039996">Call for Presentations</a></li>
+    		<li><a href="http://ruby.tw/post/5417382686">Call for Sponsors</a></li>
+    	</ul>
+    </li> 
+    <li><a href="/2010">2010/4/25, Taipei</a></li> 
+  </ul>
+  
+</div> 
+ 
+ 
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-145330-8");
+pageTracker._trackPageview();
+} catch(err) {}</script>
+ 
+</body> 
+</html>
